87ede71632a4f37fa6ebfae6779a0bc4be7a50b4
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2026-03-25 19:51:26 +0530

    refactor: Update supabase client initialization to use get_settings function

--- app/backend/db/supabase.py
+++ app/backend/db/supabase.py
@@ -1,6 +1,8 @@
 from supabase import create_client
-from app.core.settings import settings
+from backend.core.settings import get_settings
 
 def get_supabase_client():
-    print(settings)
-    raise ValueError("Supabase URL and/or Key are not set")
+    settings = get_settings()
+    if not settings.supabase_url or not settings.supabase_key:
+        raise ValueError("Supabase URL and/or Key are not set")
+    return create_client(settings.supabase_url, settings.supabase_key)
